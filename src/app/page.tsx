5246fb75203557f0319f0ea51910f132d6e3bbe8
Marie <marie.199204@gmail.com>
2023-09-27 14:29:24 +0900

    不要なタグ削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        <h1>社内向け書類アップロードサービス</h1>
-      </div>
+    <main className='flex min-h-screen flex-col items-center justify-between p-10'>
+      <h1>社内向け書類アップロードサービス</h1>
     </main>
   )
 }
